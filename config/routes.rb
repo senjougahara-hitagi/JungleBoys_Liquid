6f06958e837d1ae046504c7b9800c514be734e0c
pnam2311 <pnam2311@gmail.com>
2018-10-07 10:38:25 +0700

    sign in with username

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  root 'home#index'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    # sessions: 'users/sessions',
+    sessions: 'users/sessions',
     # password: 'users/passwords'
   }
 
