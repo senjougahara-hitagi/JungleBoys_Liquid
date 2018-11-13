0e9c90649234ac28052daa81abb34ba29ca87e38
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-11-13 02:50:28 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,21 @@ for i in 0..5
        {user_pic: 'profile-pictures/'+i+'jpg'},
        ])
 end
+
+
+100.times do
+  Room.create([
+      user_id: User.ids.sample, 
+      room_name: Faker::NatoPhoneticAlphabet.code_word,
+      address: Faker::Address.full_address,
+      cost_per_night: 3000,
+      type_of_room: 1,
+      num_of_baths: 1,
+      num_of_guests: 1,
+      num_of_bedrooms: 2,
+      num_of_beds: 5,
+      amentities: 'wifi',
+      contact_host: Faker::PhoneNumber.phone_number,
+      pictures: 'rooms/room1.jpg',
+      ])
+end
