cd986cdda0c45c6cee91a04bc54af291fcd3b6c3
Nguyễn Phương Nam <pnam2311@gmail.com>
2018-12-04 12:29:27 +0700

    read more button  (#90)
    * finalize comment section
    
    * like
    
    * room info overflow fix

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -144,10 +144,20 @@ $(document).on('turbolinks:load', function(){
     //     e.preventDefault();
     //     $(this).fadeOut('200');
     // })
+    if ($('#room_info').height() > 150){
+        $('#show_link').show();
+        $('#room_info').addClass('room-info-hide').css('overflow','hidden');
+    }
     
     $('#show_link').click(function(e){
         e.preventDefault();
-        $(this).parent().find('#room_info').toggleClass('room-info-hide');
+        rInfo = $(this).parent().find('#room_info');
+        rInfo.toggleClass('room-info-hide');
+        if (!rInfo.hasClass('room-info-hide')){
+            $(this).html('Show less <i class="fas fa-chevron-up"></i>');
+        } else {
+            $(this).html('Read more about the space <i class="fas fa-chevron-down"></i>');
+        }
     });
     
     $("#comment_form").submit(function(){
